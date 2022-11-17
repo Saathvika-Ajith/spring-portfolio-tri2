dcb1c449e8e99f429ba25765173b6324fe40a393
Saathvika-Ajith <saathvika.ajith@gmail.com>
2022-11-17 10:24:56 -0800

    frq 1 finish

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/CalendarApiController.java
@@ -37,4 +37,17 @@ public class CalendarApiController {
     }
 
     // add other methods
+    // Calendar Endpoint: /api/calendar/firstDayOfYear/2022, Returns: {"year":2020,"isLeapYear":6}
+    @GetMapping("/firstDayOfYear/{year}")
+    public ResponseEntity<JsonNode> getFirstDayOfYear(@PathVariable int year) throws JsonMappingException, JsonProcessingException {
+      // Backend Year Object
+      Year year_obj = new Year();
+      year_obj.setYear(year);  // evaluates Leap Year
+
+      // Turn Year Object into JSON
+      ObjectMapper mapper = new ObjectMapper(); 
+      JsonNode json = mapper.readTree(year_obj.firstDayOfYearToString()); // this requires exception handling
+
+      return ResponseEntity.ok(json);  // JSON response, see ExceptionHandlerAdvice for throws
+    }
 }
--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/Year.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/Year.java
@@ -8,6 +8,7 @@ package com.nighthawk.spring_portfolio.mvc.calendar;
 class Year {
    private int year;
    private boolean isLeapYear;
+   private int firstDayOfYear;
 
    // zero argument constructor
    public Year() {} 
@@ -19,6 +20,7 @@ class Year {
    public void setYear(int year) {
       this.year = year;
       this.setIsLeapYear(year);
+      this.setFirstDayOfYear(year);
    }
 
    /* isLeapYear getter/setters */
@@ -29,19 +31,32 @@ class Year {
       this.isLeapYear = APCalendar.isLeapYear(year);
    }
 
+   /* firstDayOfYear getter/setters */
+   public int getFirstDayOfYear(int year) {
+      return APCalendar.firstDayOfYear(year);
+   }
+   private void setFirstDayOfYear(int year) {  // this is private to avoid tampering
+      this.firstDayOfYear = APCalendar.firstDayOfYear(year);
+   }
+
    /* isLeapYearToString formatted to be mapped to JSON */
    public String isLeapYearToString(){
       return ( "{ \"year\": "  +this.year+  ", " + "\"isLeapYear\": "  +this.isLeapYear+ " }" );
-   }	
+   }
+   
+   /* firstDayOfYearToString formatted to be mapped to JSON */
+   public String firstDayOfYearToString(){
+      return ( "{ \"year\": "  +this.year+  ", " + "\"firstDayOfYear\": "  +this.firstDayOfYear+ " }" );
+   }
 
    /* standard toString placeholder until class is extended */
    public String toString() { 
-      return isLeapYearToString(); 
+      return isLeapYearToString() + "\n" + firstDayOfYearToString();
    }
 
    public static void main(String[] args) {
       Year year = new Year();
-      year.setYear(2022);
+      year.setYear(2024);
       System.out.println(year);
    }
 }
